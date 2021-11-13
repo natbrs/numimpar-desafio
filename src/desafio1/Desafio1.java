4029c334dbd8f64a1fe2e5b6a1802a5bc1e9a744
Natan Almeida Ramos Rezende <83055901+natbrs@users.noreply.github.com>
2021-11-13 19:36:52 -0300

    Desafio Números Ímpares - JAVA

---
+++ src/desafio1/Desafio1.java
@@ -0,0 +1,19 @@
+package desafio1;
+
+import javax.swing.JOptionPane;
+
+public class Desafio1 {
+    
+    public static void main(String[] args) {
+        
+        int i, res;
+        
+         for (i = 75; i <= 197; i++){
+         res = i % 2;
+         if (res != 0)
+            
+         System.out.println(i + " é um número ímpar");
+        
+        }
+    }
+}
